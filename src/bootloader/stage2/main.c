7380e0b57406d4fd9bc2cacf316890b045784ee4
Kap Petrov <pathaksabita31@gmail.com>
2024-09-01 14:29:00 +0530

    printf("%s", "Bonzi Buddy!");

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,9 +1,26 @@
+/*
+	Stage 2 VDK Loader
+	Written by fellow VDK Developer(s):
+	Kap Petrov
+
+	Credits:
+	- nanobyte
+*/
+
 #include "stdint.h"
 #include "stdio.h"
 
+int ageOfUser = 13;
+int build_Ver = 90124;
+
 void _cdecl cstart_(uint16_t bootDrive)
 {
-    puts("Welcome to ViaOS! VDK Version 2.1\r\n");
-    puts("Beta build 83124\r\n");
+	const char far_str = "far string";
+
+    puts("Please wait while we are loading your operating system...\r\n");
+    printf("Beta build %d\r\n", build_Ver);
+
+   	printf("You are %d years old\r\n", ageOfUser);
+
     for (;;);
 }
